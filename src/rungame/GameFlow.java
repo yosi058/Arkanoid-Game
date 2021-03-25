061a27b99e9e6f0cdef10729f958c0ef648e5fff
yosi058 <71962627+yosi058@users.noreply.github.com>
2021-03-25 10:54:24 +0200

    Update GameFlow.java

--- src/rungame/GameFlow.java
+++ src/rungame/GameFlow.java
@@ -61,6 +61,7 @@ public class GameFlow {
         this.animationRunner.run(new KeyPressStoppableAnimation(this.keyboardSensor,
                 KeyboardSensor.SPACE_KEY,
                 new EndScreen(this.win, this.counter)));
+        this.counter.setCounter(0);
     }
 }
 
